4eee6b97936713f198cbd8862e29f208971bfac1
ahmed04raxa <ahmed04raxa@gmail.com>
2026-02-14 09:26:48 +0500

    Repo Initialization

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:bookingapp/firebase_options.dart';
+import 'package:bookingapp/repository/screens/home/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BOOKING APP',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
